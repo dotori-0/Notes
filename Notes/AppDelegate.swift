2702e243827989a6097ba144637c18b8ed5216c9
dotori-0 <sesan131@gmail.com>
2022-09-05 01:49:36 +0900

    [Feat] Prevent Keyboard Sliding Up and Covering Text View

--- Notes/AppDelegate.swift
+++ Notes/AppDelegate.swift
@@ -7,6 +7,9 @@
 
 import UIKit
 
+import IQKeyboardManagerSwift
+
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -14,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        IQKeyboardManager.shared.enable = true
+        
         return true
     }
 
